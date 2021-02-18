4e6188ef62f1aa196544241eca90f14a3677cb0f
Nan Montaño <nano.mv112@gmail.com>
2021-02-18 13:41:55 -0700

    le sabes demasiado

---
+++ commands/lesabes.js
@@ -0,0 +1,11 @@
+
+/**
+ * @argument {import('discord.js').Message} message
+ * @argument {Array<any>} args
+*/
+
+module.exports = function lesabes(message, args) {
+  const channel = message.channel;
+
+  channel.send("Puta madre le sabes demasiado");
+}; 
