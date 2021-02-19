c5e2a437b5274c759251199609ea33af09d65990
Ernesto Jaramillo <nethojs29@gmail.com>
2021-02-19 13:56:15 -0700

    primer prueba de auto disconnect (8

--- commands/a.js
+++ commands/a.js
@@ -1,28 +1,36 @@
 const { allAudios } = require("../utils");
 
+let timer = null;
+
 /**
  * @argument {import('discord.js').Message} message
  * @argument {Array<any>} args
-*/
+ */
 module.exports = async function audio(message, args) {
   if (message.member.voice.channel) {
     const [audio] = args;
 
-    if(!audio) {
-      return message.channel.send('Manda el nombre de un audio puta.');
+    if (!audio) {
+      return message.channel.send("Manda el nombre de un audio puta.");
     }
 
-    const availableAudios = allAudios();
-    if(!availableAudios.includes(audio)) { return message.channel.send('No ta ese audio puta.'); }
+    timer = null;
 
-		const connection = await message.member.voice.channel.join();
+    const availableAudios = allAudios();
+    if (!availableAudios.includes(audio)) {
+      return message.channel.send("No ta ese audio puta.");
+    }
+
+    const connection = await message.member.voice.channel.join();
     const dispatcher = connection.play(`${__dirname}/../audio/${audio}.mp3`);
 
-    dispatcher.on('error', (err) => {
+    timer = setTimeout(() => message.guild.me.voice.channel.leave(), 10000);
+
+    dispatcher.on("error", (err) => {
       console.log(err);
-      message.channel.send('Error reproduciendo el audio');
+      message.channel.send("Error reproduciendo el audio");
     });
-	} else {
+  } else {
     message.channel.send("No estas en un chat de voz puta.");
   }
 };
