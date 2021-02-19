b6129f351ef614ec59a70b66db2a1258c24cd57c
Ernesto Jaramillo <nethojs29@gmail.com>
2021-02-19 14:00:38 -0700

    disconnect toma 2

--- commands/a.js
+++ commands/a.js
@@ -24,7 +24,7 @@ module.exports = async function audio(message, args) {
     const connection = await message.member.voice.channel.join();
     const dispatcher = connection.play(`${__dirname}/../audio/${audio}.mp3`);
 
-    timer = setTimeout(() => message.guild.me.voice.channel.leave(), 10000);
+    timer = setTimeout(() => connection.disconnect(), 10000);
 
     dispatcher.on("error", (err) => {
       console.log(err);
