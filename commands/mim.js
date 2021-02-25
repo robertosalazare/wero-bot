5cfffec73256aa029471c723e08aaee7571efd5b
Rolando Velez <rolandovlz@gmail.com>
2021-02-24 17:48:05 -0700

    memes

---
+++ commands/mim.js
@@ -0,0 +1,22 @@
+const axios = require('axios').default;
+
+/**
+ * @argument {import('discord.js').Message} message
+ * @argument {Array<any>} args
+*/
+module.exports = function dog(message, args) {
+  const channel = message.channel;
+  let memeApi = 'https://meme-api.herokuapp.com/gimme/';
+
+  if (args.length > 0) memeApi += args[0];
+
+  axios.get(memeApi)
+    .then((response) => {
+      const { url: url, nsfw: nsfw } = response.data;
+
+      nsfw ? channel.send('Server cristiANO :)') : channel.send(url);
+    }).catch((error) => {
+      channel.send("No encontre imagenes en ese subreddit :)");
+      console.log(error.response.data.message);
+    });
+};
